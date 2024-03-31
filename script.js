e8fc26807c7997754763d227e0c1805754015cbf
fadi-maps <165563210+fadi-maps@users.noreply.github.com>
2024-03-31 15:28:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let lamp = document.querySelector(".lamp");
+let button = document.querySelector("button");
+
+button.onclick = function () {
+  if (lamp.classList.contains("on")) {
+    lamp.classList.remove("on");
+    button.innerHTML = "off";
+    lamp.style = "box-shadow:0 0 0px 0px yellow; background-color:gray;";
+  } else {
+    lamp.classList.add("on");
+    button.innerHTML = "on";
+    lamp.style = "box-shadow:0 0 102px 10px yellow; background-color:yellow;";
+  }
+};
+let div = document.querySelector(".container");
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+let h5 = document.querySelector("h5");
+let h4 = document.createElement("h4");
+animationLoad = document.querySelector(".rotate-scroll");
+main = document.querySelector("main");
+h4.innerHTML = "h4";
+h3.after(h4);
+main.onload =function() {
+  animationLoad.style = "display:none;";
+  main.style = "display:block;";
+
+};
